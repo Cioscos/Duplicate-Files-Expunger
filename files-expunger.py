3ad66768f390f6fe2c42152199ef46e8db76c5c8
Cioscos <claudiocicca1998@gmail.com>
2022-05-02 22:48:24 +0200

    Added separate-trash
    - New CLI arg added: --separate-trash or -s to create 2 trash folders
      instead of 1.

--- files-expunger.py
+++ files-expunger.py
@@ -10,6 +10,7 @@ With dir1 you show it first directory, dir2 second directory, and those two are 
 By default it only compares file names (so asd.png and asd.jpg are considered as "the same file"), but you can use --force-extension to force it to check extension as well
 and instead of deleting files, it creates a new folder called "trash_files" and moves the files there so you can still go there and check if it "deleted" something that you wanted to keep
 '''
+from ast import arg
 import os
 import shutil
 from pathlib import Path
@@ -51,11 +52,16 @@ and instead of deleting files, it creates a new folder called "trash_files" and 
     )
     parser.add_argument('--dir1', type=str, dest='dir_1', required=True, default='directory_1', help='First folder with files')
     parser.add_argument('--dir2', type=str, dest='dir_2', required=True, default='directory_2', help='Second folder with files')
-    parser.add_argument('--trash-dir', type=str, dest='trash_dir', default='trash_files', help='Folder where deleted files will go')
-    parser.add_argument('--force-extension', action='store_true', dest='force_extension', default=False, help='Files with same name but different extension are considered different')
+    parser.add_argument('-t', '--trash-dir', type=str, dest='trash_dir', default='trash_files', help='Folder where deleted files will go')
+    parser.add_argument('-s', '--separate-trash', action='store_true', dest='separate_trash', default=False, help='Move trashed files to two different folders')
+    parser.add_argument('-f', '--force-extension', action='store_true', dest='force_extension', default=False, help='Files with same name but different extension are considered different')
     args = parser.parse_args()
 
-    os.makedirs(args.trash_dir, exist_ok=True)
+    if args.separate_trash:
+        os.makedirs(f'trash_from_{args.dir_1}', exist_ok=True)
+        os.makedirs(f'trash_from_{args.dir_2}', exist_ok=True)
+    else:
+        os.makedirs(args.trash_dir, exist_ok=True)
 
     dataset_one = make_dataset(args.dir_1)
     dataset_two = make_dataset(args.dir_2)
@@ -73,12 +79,25 @@ and instead of deleting files, it creates a new folder called "trash_files" and 
     files_to_delete = []
 
     for name in final_names:
-        for path in dataset_one + dataset_two:
-            if name == (path.name if args.force_extension else path.stem):
-                files_to_delete.append(path)
+        if args.separate_trash:
+            for path in dataset_one:
+                if name == (path.name if args.force_extension else path.stem):
+                    files_to_delete.append((1, path))
+            for path in dataset_two:
+                if name == (path.name if args.force_extension else path.stem):
+                    files_to_delete.append((2, path))
+        else:    
+            for path in dataset_one + dataset_two:
+                if name == (path.name if args.force_extension else path.stem):
+                    files_to_delete.append(path)
+
     
-    for path in tqdm(files_to_delete, desc='Moving files'):
-        shutil.move(path, args.trash_dir)
+    if args.separate_trash:
+        for path in tqdm(files_to_delete, desc='Moving files'):
+            shutil.move(path[1], f'trash_from_{args.dir_1 if path[0] == 1 else args.dir_2}')
+    else:
+        for path in tqdm(files_to_delete, desc='Moving files'):
+            shutil.move(path, args.trash_dir)
 
 
 if __name__ == '__main__':
